66526c5dfdc728ccd62a2e1fc6065943e5253126
angelwayar <angelmanuelwayar@gmail.com>
2019-09-09 18:43:53 -0400

    Hola

--- app/src/main/java/com/axon/pruebabilletera/MainActivity.kt
+++ app/src/main/java/com/axon/pruebabilletera/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.axon.pruebabilletera
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var prueba :String = "hola"
         setContentView(R.layout.activity_main)
     }
 }
