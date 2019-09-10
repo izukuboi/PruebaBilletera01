d3a59ae30558b40d68a048a797f29d1deb60e735
angelwayar <angelmanuelwayar@gmail.com>
2019-09-10 08:51:05 -0400

    Home_Activity creado

---
+++ app/src/main/java/com/axon/pruebabilletera/Home_Activity.kt
@@ -0,0 +1,12 @@
+package com.axon.pruebabilletera
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Home_Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_)
+    }
+}
--- app/src/main/java/com/axon/pruebabilletera/MainActivity.kt
+++ app/src/main/java/com/axon/pruebabilletera/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.axon.pruebabilletera
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +11,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         var prueba :String = "hola"
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener{
+            val intent: Intent = Intent(this, Home_Activity::class.java)
+            startActivity(intent)
+            
+        }
     }
 }
